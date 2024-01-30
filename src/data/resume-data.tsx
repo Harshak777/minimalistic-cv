54857997a769d5e47425e865d762e12e05571c5b
Harshak777 <harshakkrishnaa777@gmail.com>
2024-01-30 12:43:47 -0700

    Removed console

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -1,7 +1,5 @@
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
-console.log("Google id", process.env.GA_ID as String[0])
-
 export const RESUME_DATA = {
   name: "Harshak Krishnaa Keerthipati",
   initials: "KHK",
